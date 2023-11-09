07662c196d1d9b4bd149f8e2d473b652ef8456e3
sardorbek23 <torayevsardorbek229@gmail.com>
2023-11-09 19:34:51 +0500

    first

---
+++ main/main.js
@@ -0,0 +1,44 @@
+var Name = String(prompt("Ismingiz:"));
+console.log(Name);
+
+var Surname = String(prompt("Familiyangiz:"));
+console.log(Surname);
+
+var Age = Number(prompt("Yoshingiz:"));
+console.log(Age);
+
+var Phone = Number(prompt("Telefon raqamingiz:"));
+console.log(Phone);
+
+var Job = String(prompt("Kasbingiz:"));
+console.log(Job);
+
+var Sport = String(prompt("Sevimli sportingiz:"));
+console.log(Sport);
+
+var Music = String(prompt("Sevimli musiqangiz:"));
+console.log(Music);
+
+var Film = String(prompt("Sevimli filmingiz:"));
+console.log(Film);
+
+var Hobby = String(prompt("Hobbiyingiz"));
+console.log(Hobby);
+
+var Address = String(prompt("Manzilingiz:"));
+console.log(Address);
+
+var Answers = [
+    "Name:"+""+Name+"\n"+
+    "Surname:"+""+Surname+"\n"+
+    "Age:"+""+Age+"\n"+
+    "Phone:"+""+Phone+"\n"+
+    "Job:"+""+Job+"\n"+
+    "Sport:"+""+Sport+"\n"+
+    "Music:"+""+Music+"\n"+
+    "Film:"+""+Film+"\n"+
+    "Hobby:"+""+Hobby+"\n"+
+    "Address:"+""+Address+"\n"
+];
+
+alert(Answers);
