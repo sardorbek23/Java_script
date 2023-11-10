7b3f90948552b2db9f52e5cc143cdc23145d9500
Sardor23 <147434376+sardorbek23@users.noreply.github.com>
2023-11-10 19:48:04 +0500

    Add files via upload

---
+++ dars-3-6/js/main.js
@@ -0,0 +1,11 @@
+var pul = Number (prompt("Summani kiriting"));
+var heading =document.querySelector (".heading");
+if(8317488.6 <= pul){
+    heading.textContent = ("Oq yo'l Alisher")
+}
+else {
+    heading.textContent = ("Sabr qilasiz Alisher")
+}
+
+
+
